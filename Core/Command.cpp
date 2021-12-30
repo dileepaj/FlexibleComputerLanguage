1df9a58249efebb968ebc3e136e9bc1280e17c2b
upulka <67848552+upulka@users.noreply.github.com>
2021-12-30 16:06:54 +0530

    TRAC-3597-Method implementation

--- Core/Command.cpp
+++ Core/Command.cpp
@@ -900,6 +900,19 @@ PENTITY Command::ExecuteNodeCommand(MULONG ulCommand, PENTITY pEntity, Execution
         
         switch(ulCommand)
         {
+            case COMMAND_TYPE_SET_INNER_ARRAY : {
+                if(ENTITY_TYPE_STRING == pArg->ul_Type){
+                    String* pStrArg = (String*)pArg;
+                    if(pStrArg != 0 ){
+                        MemoryManager::Inst.CreateObject(&pNullRes);
+                        pNodeRes = pNode->AddNode();
+                        pNodeRes->SetCustomString("array");
+                        pNodeRes->SetLValue((PMCHAR)pStrArg->GetValue().c_str());
+                    }
+                }
+                break;
+            }
+
             case COMMAND_TYPE_ADD_INNER_OBJ: {
                 if(ENTITY_TYPE_STRING == pArg->ul_Type){
                     String* pStrArg = (String*)pArg;
