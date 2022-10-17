78f2b42327a33f4f58c65d1cceef1fd069aae205
upulka <upulka.saku@gmail.com>
2022-10-17 17:08:19 +0530

    TRAC-6469-Fix the build issue

--- Core/Command.cpp
+++ Core/Command.cpp
@@ -745,14 +745,16 @@ PENTITY Command::ExecuteNodeCommand(MULONG ulCommand, PENTITY pEntity, Execution
         switch(ulCommand)
         {
             case COMMAND_TYPE_SET_INNER_ARRAY : {
-                if(ENTITY_TYPE_STRING == pArg->ul_Type){
-                    String* pStrArg = (String*)pArg;
-                    if(pStrArg != 0 ){
+                if (ENTITY_TYPE_STRING == pArg->ul_Type) {
+                    String *pStrArg = (String *) pArg;
+                    if (pStrArg != 0) {
                         MemoryManager::Inst.CreateObject(&pNullRes);
                         pNodeRes = pNode->AddNode();
                         pNodeRes->SetCustomString("array");
-                        pNodeRes->SetLValue((PMCHAR)pStrArg->GetValue().c_str());
-
+                        pNodeRes->SetLValue((PMCHAR) pStrArg->GetValue().c_str());
+                    }
+                }
+            }
             case COMMAND_TYPE_SET_NORMAL_STRING : {
                 if(ENTITY_TYPE_LIST == pArg->ul_Type){
                     String* pStrArg = (String*)pArg;
